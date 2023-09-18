4de2a44885defca77567ee10c06e94b03682204a
Johann Heinzelreiter <Johann.Heinzelreiter@fh-hagenberg.at>
2023-09-18 20:43:14 +0200

    Adapt STUDENT_DATA_PATTERN so that all unicode characters can occur in first names and last names

--- src/TutorBot.Cli/Constants.cs
+++ src/TutorBot.Cli/Constants.cs
@@ -4,7 +4,7 @@ internal static class Constants
 {
   public const string APP_NAME = "gh-tutorbot";
   public const string ROSTER_FILE_PATH = @"./classroom_roster.csv";
-  public const string STUDENT_DATA_PATTERN = @"^(?<LastName>[A-ZÄÖÜa-zäöüß\.\-]+)\s+(?<FirstName>([A-ZÄÖÜa-zäöüß\.\-\s]*[A-ZÄÖÜa-zäöüß\.\-]))\s*\(G(?<GroupNr>\d)/(?<MatNr>S\d+)\)$";
+  public const string STUDENT_DATA_PATTERN = @"^(?<LastName>[\w\.\-]+)\s+(?<FirstName>([\w\.\-\s]*[\w\.\-]))\s*\(G(?<GroupNr>\d)/(?<MatNr>S\d+)\)$";
   // "LastName FirstName1 FirstName2 ... (G9/S999999999)";
   public const string GITHUB_READ_ROLE = "read";
   public const string GITHUB_WRITE_ROLE = "write";
