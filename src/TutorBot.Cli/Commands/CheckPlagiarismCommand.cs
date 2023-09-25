3abe1014d9b6560c6fd0ffe51c8f5a8c6a27f783
Johann Heinzelreiter <Johann.Heinzelreiter@fh-hagenberg.at>
2023-09-25 17:55:26 +0200

    Extend check-plagiarism command: show results of check

--- src/TutorBot.Cli/Commands/CheckPlagiarismCommand.cs
+++ src/TutorBot.Cli/Commands/CheckPlagiarismCommand.cs
@@ -1,6 +1,9 @@
-﻿using System.CommandLine;
+﻿using System;
+using System.CommandLine;
+using System.IO.Compression;
 using Microsoft.Extensions.Logging;
 using Octokit;
+using TutorBot.Domain.JPlag;
 using TutorBot.Infrastructure;
 using TutorBot.Infrastructure.Exceptions;
 using TutorBot.Infrastructure.OctokitExtensions;
@@ -20,8 +23,9 @@ internal class CheckPlagiarismCommand : Command
   private readonly Argument<string> rootDirectoryArgument = new("root-directory", "root directory containing submissions");
   private readonly Option<string> languageOption = new("--language", "language");
   private readonly Option<string> reportFileOption = new("--report-file", "name of the file in which the comparison results will be stored");
+  private readonly Option<bool> refreshOption = new("--refresh", "redo check although results file exists");
 
-  private async Task HandleAsync(string rootDirectory, string languageOption, string? reportFileOption)
+  private async Task HandleAsync(string rootDirectory, string languageOption, string? reportFileOption, bool refreshOption)
   {
     try
     {
@@ -30,12 +34,35 @@ internal class CheckPlagiarismCommand : Command
         throw new LogicException($"Error: Root directory \"{rootDirectory}\" does not exist. Clone assignment first.");
       }
 
-      if (!File.Exists(configuration.JplagJarPath))
+      string reportFile = reportFileOption ?? $"{rootDirectory}/{Constants.DEFAULT_REPORT_FILE}";
+      string jplugResultFile = $"{reportFile}.zip";
+
+      bool resultFileExists;
+      if (refreshOption || !File.Exists(jplugResultFile))
       {
-         throw new LogicException($"Error: JPlag jar file \"{rootDirectory}\" does not exist. Download JPlag from https://github.com/jplag/JPlag/releases.\n" + 
-                                  $"       Ensure that the configuration parameter \"{ConfigurationHelper.KEY_JPLAG_JAR_PATH}\" is set appropriately.");
+        resultFileExists = await RunJplag(rootDirectory, reportFile, languageOption);
+      }
+      else
+      {
+        resultFileExists = true;
+        Console.WriteLine($"Info: JPlag results file \"{jplugResultFile}\" already exists. Skipping plagiarism check.");
       }
 
+      if (resultFileExists)
+      {
+        await PrintJPlagResults(jplugResultFile);
+      }
+    }
+    catch (Exception ex)
+    {
+      ExceptionHelper.HandleException(ex);
+    }
+  }
+
+  private async Task<bool> RunJplag(string rootDirectory, string reportFile, string languageOption)
+  {
+    try
+    {
       string language = languageOption switch
       {
         "cpp" => "cpp2",
@@ -43,7 +70,11 @@ internal class CheckPlagiarismCommand : Command
         _ => throw new LogicException($"Error: Unknown language \"{languageOption}\"")
       };
 
-      string reportFile = reportFileOption ?? $"{rootDirectory}/{Constants.DEFAULT_REPORT_FILE}";
+      if (!File.Exists(configuration.JplagJarPath))
+      {
+        throw new LogicException($"Error: JPlag jar file \"{rootDirectory}\" does not exist. Download JPlag from https://github.com/jplag/JPlag/releases.\n" +
+                                 $"       Ensure that the configuration parameter \"{ConfigurationHelper.KEY_JPLAG_JAR_PATH}\" is set appropriately.");
+      }
 
       var jplagArgs = string.Format(Constants.JPLAG_ARGS, language, reportFile, rootDirectory);
       var javaArgs = $"-jar \"{configuration.JplagJarPath}\" {jplagArgs}";
@@ -58,17 +89,47 @@ internal class CheckPlagiarismCommand : Command
         Console.WriteLine(result);
         Console.Error.WriteRedLine($"{errorResult}");
       }
+
+      return exitCode == 0;
     }
     catch (CommandNotFoundException)
     {
-      Console.Error.WriteRedLine($"Error: Java (\"{configuration.JavaPath}\") not found");
-    }
-    catch (Exception ex)
-    {
-      ExceptionHelper.HandleException(ex);
+      throw new LogicException($"Error: Java (\"{configuration.JavaPath}\") not found.");
     }
   }
 
+  private static async Task PrintJPlagResults(string resultsZipFile)
+  {
+    try
+    {
+      var archive = ZipFile.Open(resultsZipFile, ZipArchiveMode.Read);
+
+      ZipArchiveEntry ? entry = archive.GetEntry("overview.json");
+      if (entry is not null)
+      {
+        using (var jsonStream = entry.Open())
+        {
+          var overviewDocument = await JPlagOverviewDocument.FromJsonAsync(jsonStream);
+
+          var printer = new TablePrinter();
+          printer.AddRow("STUDENT-1", "STUDENT-2", "SIMILARITY");
+
+          foreach (var comparision in overviewDocument.Metrics[0].TopComparisons)
+          {
+            printer.AddRow($"{comparision.FirstSubmission}",
+                            $"{comparision.SecondSubmission}",
+                            FormattableString.Invariant($"{comparision.Similarity,9:F2}"));
+          }
+
+          printer.Print();
+        }
+      }
+    }
+    catch (FileNotFoundException ex)
+    {
+      throw new LogicException($"Error: JPlag results file \"{ex.FileName}\" not found.");
+    }
+  }
 
   public CheckPlagiarismCommand(IGitHubClient client, ConfigurationHelper configuration, ILogger<ListAssignmentsCommand> logger) :
     base("check-plagiarism", "Check for plagiarism")
@@ -83,12 +144,17 @@ internal class CheckPlagiarismCommand : Command
               .SetDefaultValue("cpp");
     AddOption(languageOption);
 
-    reportFileOption.AddAlias("-r");
+    reportFileOption.AddAlias("-rf");
     AddOption(reportFileOption);
+
+    refreshOption.AddAlias("-r");
+    refreshOption.SetDefaultValue(false);
+    AddOption(refreshOption);
+
 
     AddAlias("cp");
 
-    this.SetHandler(HandleAsync, rootDirectoryArgument, languageOption, reportFileOption);
+    this.SetHandler(HandleAsync, rootDirectoryArgument, languageOption, reportFileOption, refreshOption);
   }
 }
 
---
+++ src/TutorBot.Cli/Domain/JPlag/JplagResultParser.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TutorBot.Logic.Exceptions;
+
+namespace TutorBot.Domain.JPlag;
+
+
+public class JplagVersion
+{
+  [JsonPropertyName("major")]
+  public int Major { get; set; }
+
+  [JsonPropertyName("minor")]
+  public int Minor { get; set; }
+
+  [JsonPropertyName("patch")]
+  public int Patch { get; set; }
+}
+
+public class JPlagComparison
+{
+  [JsonPropertyName("first_submission")]
+  public string? FirstSubmission { get; set; }
+
+  [JsonPropertyName("second_submission")]
+  public string? SecondSubmission { get; set; }
+
+  [JsonPropertyName("similarity")]
+  public double Similarity { get; set; }
+}
+
+public class JPlagMetric
+{
+  [JsonPropertyName("name")]
+  public string? Name { get; set; }
+
+  [JsonPropertyName("distribution")]
+  public List<int> Distribution { get; set; } = new();
+
+  [JsonPropertyName("topComparisons")]
+  public List<JPlagComparison> TopComparisons { get; set; } = new();
+
+  [JsonPropertyName("description")]
+  public string? Description { get; set; }
+}
+
+public class JPlagOverviewDocument
+{
+  public static async Task<JPlagOverviewDocument> FromJsonAsync(Stream jsonStream)
+  {
+    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    return (await JsonSerializer.DeserializeAsync<JPlagOverviewDocument>(jsonStream, options)) ?? throw new LogicException("Could not deserialize JSON");
+  }
+
+  [JsonPropertyName("jplag_version")]
+  public JplagVersion? JplagVersion { get; set; }
+
+  [JsonPropertyName("submission_folder_path")]
+  public List<string> SubmissionFolderPath { get; set; } = new();
+
+  [JsonPropertyName("base_code_folder_path")]
+  public string? BaseCodeFolderPath { get; set; }
+
+  [JsonPropertyName("language")]
+  public string? Language { get; set; }
+
+  [JsonPropertyName("file_extensions")]
+  public List<string> FileExtensions { get; set; } = new();
+
+  [JsonPropertyName("submission_id_to_display_name")]
+  public Dictionary<string, string>? SubmissionIdToDisplayName { get; set; }
+
+  [JsonPropertyName("submission_ids_to_comparison_file_name")]
+  public Dictionary<string, Dictionary<string, string>>? SubmissionIdsToComparisonFileName { get; set; }
+
+  [JsonPropertyName("failed_submission_names")]
+  public List<string> FailedSubmissionNames { get; set; } = new();
+
+  [JsonPropertyName("excluded_files")]
+  public List<string> ExcludedFiles { get; set; } = new();
+
+  [JsonPropertyName("match_sensitivity")]
+  public int MatchSensitivity { get; set; }
+
+  [JsonPropertyName("date_of_execution")]
+  public string? DateOfExecution { get; set; }
+
+  [JsonPropertyName("execution_time")]
+  public int ExecutionTime { get; set; }
+
+  [JsonPropertyName("metrics")]
+  public List<JPlagMetric> Metrics { get; set; } = new();
+
+  [JsonPropertyName("clusters")]
+  public List<string> Clusters { get; set; } = new();
+
+  [JsonPropertyName("total_comparisons")]
+  public int TotalComparisons { get; set; }
+}
+
