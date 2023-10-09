f0ac705c0a2bd0ce1411c17a6c397cc8235f5ccc
Johann Heinzelreiter <Johann.Heinzelreiter@fh-hagenberg.at>
2023-10-10 00:02:54 +0200

    Add --group option to list-students

--- src/TutorBot.Cli/Commands/ListStudentsCommand.cs
+++ src/TutorBot.Cli/Commands/ListStudentsCommand.cs
@@ -6,7 +6,9 @@ namespace TutorBot.Commands;
 
 internal class ListStudentsCommand : Command
 {
-  private async Task HandleAsync()
+  private readonly Option<int?> groupOption = new("--group", "filter group");
+
+  private async Task HandleAsync(int? group)
   {
     var printer = new TablePrinter();
     printer.AddRow("LASTNAME", "FIRSTNAME", "STUD.ID", "GITHUBNAME", "GROUPNR.");
@@ -15,12 +17,22 @@ internal class ListStudentsCommand : Command
     {
       var studentList = await StudentList.FromRoster(Constants.ROSTER_FILE_PATH);
 
-      foreach (var student in studentList.LinkedStudents)
+      var filteredlinkedStudents = studentList.LinkedStudents;
+      if (group is not null)
+      {
+        filteredlinkedStudents = filteredlinkedStudents.Where(s => s.GroupNr == group).ToList();
+      }
+      foreach (var student in filteredlinkedStudents)
       {
         printer.AddRow(student.LastName, student.FirstName, student.MatNr, student.GitHubUsername, student.GroupNr.ToString());
       }
 
-      foreach (var student in studentList.UnlinkedStudents)
+      var filteredUnLinkedStudents = studentList.UnlinkedStudents;
+      if (group is not null)
+      {
+        filteredUnLinkedStudents = filteredUnLinkedStudents.Where(s => s.GroupNr == group).ToList();
+      }
+      foreach (var student in filteredUnLinkedStudents)
       {
         printer.AddRow(student.LastName, student.FirstName, student.MatNr, "-", student.GroupNr.ToString());
       }
@@ -36,9 +48,13 @@ internal class ListStudentsCommand : Command
   public ListStudentsCommand() :
   base("list-students", "List all students")
   {
+    groupOption.AddAlias("-g");
+    groupOption.SetDefaultValue(null);
+    AddOption(groupOption);
+
     this.AddAlias("lstud");
 
-    this.SetHandler(HandleAsync);
+    this.SetHandler(HandleAsync, groupOption);
   }
 }
 
